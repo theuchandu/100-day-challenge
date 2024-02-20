8d207b898eb5a08cc961687e1de695424c430604
Godishala Chandu <85674113+theuchandu@users.noreply.github.com>
2024-02-20 23:58:50 +0530

    started 100 day challenge

---
+++ index.php
@@ -0,0 +1,30 @@
+
+ <!-- Procedural Style: -->
+
+<!-- /* <?php
+
+$name  = "John Doe";
+$age   =   "24" ;
+
+echo  "my name is".$name."and my age is ".$age ;
+
+?> */ -->
+
+<!-- <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>PHP Example</h1> 
+    <?php
+        $name = "jhon";
+        $age = "23";
+
+         echo "<p> Name : $name </p>";
+         echo "<p> Age : $age </p>";
+    ?>
+</body>
+</html> -->
