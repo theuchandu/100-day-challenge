50b2270d6b4534471e529fbd1d26d6c80d596473
Godishala Chandu <85674113+theuchandu@users.noreply.github.com>
2024-02-20 23:21:06 +0530

    Today I completed 5 javacript prgrams

---
+++ Day1.js
@@ -0,0 +1,63 @@
+// //programm to add to numbers in js 
+
+// var a = 1 ;
+// var b = 2;
+// console.log(a+b);
+
+// //program to find to multiplication oof twonumbers 
+//  var a = 12; 
+//  var b = 14; 
+//  console.log(a*b);
+
+//JavaScript Program to Add Float Numbers
+
+// function num() {
+//     console.log(parseFloat('1.5') + parseFloat('2.5').toFixed(2));
+// }
+
+// num();
+// 4 JavaScript Program to Check if a Number is Odd or Even--
+// var num1 = 34 ;
+// var num2 = 27; 
+// if(num1 % 2 === 0) {
+//     console.log("Number "+num1+" is even");
+// }else{
+//     console.log("Number"+num2+ "is odd");
+// }
+
+// if (num2 % 2 === 0) {
+//     console.log("Number " + num2 + " is even");
+// } else {
+//     console.log("Number " + num2 + " is odd");
+// }
+
+// 5 JavaScript Program to Check if a number is Positive, Negative, or Zero
+
+//postive
+
+// var num1 = 12 ;
+// var num2 = -13;
+// var num3 = 0 ;
+
+// if(num1 > 0 ){
+//     console.log("number" + num1 + "is positive");
+// }else if(num1 < 0){
+//     console.log("number" + num1 + "is negative");
+// }else{
+//     console.log("Given number is 0");
+// }
+// if(num2 > 0 ){
+//     console.log("number" + num2 + "is positive");
+// }else if(num2 < 0){
+//     console.log("number" + num2 + "is negative");
+// }else{
+//     console.log("Given number is 0");
+// }
+// if(num3 > 0 ){
+//     console.log("number" + num3 + "is positive");
+// }else if(num3 < 0){
+//     console.log("number" + num3 + "is negative");
+// }else{
+//     console.log("Given number is 0");
+// }
+
